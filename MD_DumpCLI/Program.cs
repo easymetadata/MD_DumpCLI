24c17214298ae81fb659c7b29a147bd6d7046831
easymetadata <ddym@redrocktx.com>
2016-12-30 14:51:44 -0600

    1.0.0
    Initial release.

---
+++ MD_DumpCLI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using CommandLine;
+
+namespace MD_DumpCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            if (!result.Errors.Any())
+            {
+                Console.WriteLine("Author:  David Dym");
+                Console.WriteLine("License: Apache 2.0\n https://www.apache.org/licenses/");
+
+                // Values are available here
+                if (result.Value.Verbose) Console.WriteLine("\nFilename: {0}", result.Value.InputFile);
+            }
+
+            //Kickoff processing and collection of files!
+            TikaMetaData.TikaExtractAll(result.Value.InputFile, result.Value.content);
+        }
+    }
+
+    class Options
+    {
+        [Option('f', "file", Required = true,
+          HelpText = "File to dump metadata.")]
+        public string InputFile { get; set; }
+
+        [Option('c', "Output content using Tika", Required = false,
+            HelpText = "Output file or metadata.")]
+        public bool content { get; set; }
+
+        // omitting long name, default --verbose
+        [Option(DefaultValue = true,
+          HelpText = "Usage: MD_DumpCLI.exe -i \"filename.ext\" -c True ")]
+        public bool Verbose { get; set; }
+    }
+}
---
+++ MD_DumpCLI/TikaMetaData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TikaOnDotNet.TextExtraction;
+
+namespace MD_DumpCLI
+{
+    public class TikaMetaData
+    {
+        public static string TikaText = string.Empty;
+
+        public static void TikaExtractAll(string _filename, bool bGiveContent = false)
+        {
+            var results = new Dictionary<string, string>();
+
+            try
+            {
+                var textExtractor = new TextExtractor();
+                var result = textExtractor.Extract(_filename);
+
+                foreach (var dict in result.Metadata.OrderBy(x => x.Key))
+                {
+                        try
+                        {
+                            Console.Write("\n{0}: ", dict.Key.ToString());
+                            Console.Write("{0}", dict.Value ?? string.Empty);
+                            //Console.WriteLine("\n------");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Exception", ex.Message);
+                        }
+                }
+
+                if(bGiveContent)   
+                   Console.Write(result.Text.Trim());
+
+                result = null;
+                textExtractor = null;
+            }
+
+            catch (TikaOnDotNet.TextExtraction.TextExtractionException tex)
+            {
+                Console.WriteLine("Tika Exception", tex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception", ex.Message);
+            }
+
+            
+        }
+    }
+}
